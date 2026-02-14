a5af5b621ef330c44a155ac2bd48355085231687
leisiji <2265215145@qq.com>
2026-02-14 16:13:19 +0800

    fix remove_unref_sym.sh not strip symbols

--- remove_unref_sym.sh
+++ remove_unref_sym.sh
@@ -6,5 +6,5 @@ ${CROSS_COMPILE}strip --strip-unneeded $1 && \
 ${CROSS_COMPILE}readelf -r -W $1 | awk 'NR>3' | awk '{print $5}'  | sort | uniq > ref.txt && \
 ${CROSS_COMPILE}readelf -s -W $1 | awk 'NR>3' |  awk '{print $8}'  | sort | uniq > syms.txt && \
 diff ref.txt syms.txt | grep "^>" | grep -v "\$t" | grep -v "\$d" | awk '{print $2}' > unused.txt && \
-if [ ! -s unused.txt ]; then llvm-objcopy --strip-symbols unused.txt $1; fi;
+llvm-objcopy --strip-symbols unused.txt $1;
 rm -rf ref.txt syms.txt unused.txt
